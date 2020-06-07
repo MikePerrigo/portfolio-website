631b52ccf8122c970d4460915090009115eadb27
Mike Perrigo <michael.perrigo@gmail.com>
2020-06-07 14:50:54 -0400

    Completed Contact Page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
 class Contact extends React.Component {
     render() {
@@ -13,10 +13,29 @@ class Contact extends React.Component {
                             alt="placeholder"
                             style={{height: '400px'}}
                             />
-                            <p>This is sample text</p>
+                            <p style={{width:'75%', margin:'auto', paddingTop: '1em'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+                                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+                                mollit anim id est laborum.</p>
                         </Cell>
                         <Cell col={6}>
-                            Half Page
+                            <h2>Contact Me</h2>
+                            <hr/>
+                            <div className="contact-list">
+                                <List>
+                                    <ListItem>
+                                        <ListItemContent style={{fontSize: '30px', fontFamily:'Anton'}}>
+                                            <i className="fa fa-phone-square" aria-hidden="true"/>
+                                            (978) 319-1523</ListItemContent>
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListItemContent style={{fontSize: '30px', fontFamily:'Anton'}}>
+                                            <i className="fa fa-envelope-square" aria-hidden="true"/>
+                                            michael.perrigo@gmail.com</ListItemContent>
+                                    </ListItem>
+                                </List>
+                            </div>
                         </Cell>
                     </Grid>
                 </div>
