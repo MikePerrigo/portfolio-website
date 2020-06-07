81e9f7802a9d130c92996f75bc2ca69d76e52a94
Mike Perrigo <michael.perrigo@gmail.com>
2020-06-07 12:07:52 -0400

    Added Projects updates

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,62 @@
 import React, { Component } from 'react';
+import { Tab, Tabs, Grid, Cell, Card, CardTitle, CardActions, Button, CardMenu, IconButton, CardText } from 'react-mdl';
 
 class Projects extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeTab: 0
+        };
+    }
+
+toggleCategories() {
+
+    if(this.state.activeTab ===0) {
+        return(
+            <div className="projects-grid">
+                {/* Project 1 */}
+                <Card shadow={5} style={{width: '512px', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://create-react-app.dev/img/logo-og.png) center / cover'}}>Sprint Drink Generator</CardTitle>
+                    <CardText>Sprint Drink generator</CardText>
+                    <CardActions border>
+                        <Button colored>GitHub</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+                {/* Project 2 */}
+                <Card shadow={5} style={{width: '512px', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://create-react-app.dev/img/logo-og.png) center / cover'}}>Portfolio Website</CardTitle>
+                    <CardText>Portfolio website build via React</CardText>
+                    <CardActions border>
+                        <Button colored>GitHub</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+            </div>
+        )
+    }else if(this.state.activeTab ===1) {
+        return(
+            <div><h1>This is Python</h1></div>
+        )
+    }
+}
     render() {
         return(
-            <div>
-                <h1>Projects Page</h1>
+            <div className="category-tabs">
+                <Tabs activeTab = {this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId})} ripple>
+                    <Tab>React</Tab>
+                    <Tab>Python</Tab>
+                </Tabs>
+
+                <Grid>
+                    <Cell col={12}>
+                        <div className="content">{this.toggleCategories()}</div>
+                    </Cell>
+                </Grid>
             </div>
         )
     }
